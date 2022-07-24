bf12bf93109eb240b0acad20b1a7dbdbd1df4fd0
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-23 21:06:19 -0700

    append slice

--- cards/main.go
+++ cards/main.go
@@ -6,6 +6,7 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades"
 	cards := []string { newCard(), "Ace of Diamonds" }
+	cards = append(cards, "Six of Spades")
 
 	fmt.Println(cards)
 }
