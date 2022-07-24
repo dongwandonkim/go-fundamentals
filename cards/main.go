cf42915b35c491f659d501ea6190f9042ebdf47e
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-24 15:12:42 -0700

    newDeck

--- cards/main.go
+++ cards/main.go
@@ -1,20 +1,9 @@
 package main
 
-import "fmt"
-
-
 func main() {
 	// var card string = "Ace of Spades"
-	cards := deck { newCard(), "Ace of Diamonds" }
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	     return "Five of Diamonds"
-
 
 }
