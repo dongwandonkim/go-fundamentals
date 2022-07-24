6edc858eeedc381ff7e4465c3df3173f70dbd2f4
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-24 16:43:31 -0700

    deck toString

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // create a new type of 'deck'
 // which is a slice of strings
@@ -30,4 +33,9 @@ func (d deck) print() {
 // return 2 separate value
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+
+	return strings.Join([]string(d), ",")
 }
--- cards/main.go
+++ cards/main.go
@@ -3,17 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	// cards := newDeck()
-
-	// // cards.print()
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-
-	greeting := "Hi there"
-	// type conversion
-	fmt.Println([]byte(greeting))
+	cards := newDeck()
+	fmt.Println(cards.toString())
 
 }
