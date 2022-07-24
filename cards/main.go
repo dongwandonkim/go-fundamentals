98039cee18fb2194e61ae072965ba810d3c64b41
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-23 21:05:11 -0700

    create slice

---
+++ cards/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// var card string = "Ace of Spades"
+	cards := []string { newCard(), "Ace of Diamonds" }
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	     return "Five of Diamonds"
+
+
+}
