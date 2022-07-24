c918e104e2866b1d0990ff39d54a357b11413e4b
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-24 16:32:22 -0700

    type conversion

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var card string = "Ace of Spades"
-	cards := newDeck()
+	// cards := newDeck()
 
-	// cards.print()
-	hand, remainingCards := deal(cards, 5)
+	// // cards.print()
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	greeting := "Hi there"
+	// type conversion
+	fmt.Println([]byte(greeting))
 
 }
